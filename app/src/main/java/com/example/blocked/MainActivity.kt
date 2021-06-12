33f7b15f50631d76486b5f4c5771fbcfa278a1e4
Max Tyler <maxastyler@gmail.com>
2021-06-12 06:41:49 +0100

    Initial commit

---
+++ app/src/main/java/com/example/blocked/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.blocked
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collection.mutableVectorOf
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.blocked.ui.theme.BlockedTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BlockedTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    BlockedTheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/com/example/blocked/game/Vec2.kt
@@ -0,0 +1,42 @@
+package com.example.blocked.game
+
+sealed class Rotation {
+    object Left : Rotation()
+    object Right : Rotation()
+    object None : Rotation()
+    object Half : Rotation()
+
+    fun toInt() = when (this) {
+        None -> 0
+        Right -> 1
+        Half -> 2
+        Left -> 3
+    }
+
+    companion object {
+        fun fromInt(i: Int): Rotation = when (Math.floorMod(i, 4)) {
+            0 -> None
+            1 -> Right
+            2 -> Half
+            3 -> Left
+            else -> throw ArithmeticException()
+        }
+    }
+
+    operator fun plus(other: Rotation): Rotation = Rotation.fromInt(this.toInt() + other.toInt())
+}
+
+
+data class Vec2(val x: Int, val y: Int) {
+    operator fun plus(other: Vec2): Vec2 = Vec2(this.x + other.x, this.y + other.y)
+    operator fun minus(other: Vec2): Vec2 = Vec2(this.x - other.x, this.y - other.y)
+    operator fun times(other: Int): Vec2 = Vec2(this.x * other, this.y * other)
+
+    fun rotate(rotation: Rotation): Vec2 = when (rotation) {
+        Rotation.None -> this
+        Rotation.Right -> Vec2(this.y, -this.x)
+        Rotation.Half -> Vec2(-this.x, -this.y)
+        Rotation.Left -> Vec2(-this.y, this.x)
+    }
+
+}
