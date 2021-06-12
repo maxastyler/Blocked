164ef460726b6182208b6d5513ee837883a6f83e
Max Tyler <maxastyler@gmail.com>
2021-06-12 06:48:10 +0100

    Add some tests

---
+++ app/src/test/java/com/example/blocked/game/RotationTest.kt
@@ -0,0 +1,16 @@
+package com.example.blocked.game
+
+import junit.framework.TestCase
+
+class RotationTest : TestCase() {
+
+    fun testToInt() {
+        assertEquals(Rotation.fromInt(Rotation.Left.toInt() + Rotation.Right.toInt()), Rotation.None)
+    }
+
+    fun testPlus() {
+        assertEquals(Rotation.Left + Rotation.Right, Rotation.None)
+        assertEquals(Rotation.Half + Rotation.Right + Rotation.Left, Rotation.Half)
+        assertEquals(Rotation.None + Rotation.None, Rotation.None)
+    }
+}
