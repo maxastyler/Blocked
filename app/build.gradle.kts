62926ca24e9ca2fb11b4aadd6d588606e7ee1c60
Max Tyler <maxastyler@gmail.com>
2021-06-27 11:52:20 +0100

    Change version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.maxtyler.blocked"
         minSdk = 26
         targetSdk = 30
-        versionCode = 3
-        versionName = "1.2"
+        versionCode = 4
+        versionName = "1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
